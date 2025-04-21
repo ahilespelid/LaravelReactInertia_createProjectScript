f0b209ff36c67f7384afd105377da2e83c4e5936
Ahi Les <47417767+ahilespelid@users.noreply.github.com>
2025-04-21 17:15:48 +0300

    Update LaravelReactInertia_createProjectScript.sh

--- LaravelReactInertia_createProjectScript.sh
+++ LaravelReactInertia_createProjectScript.sh
@@ -263,7 +263,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->float('wallet')->nullable();
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
             $table->string('login')->unique();
             $table->string('password');
             $table->boolean('exchanger')->nullable();
